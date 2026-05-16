258ba09eaa6c1a756ca55647d62501ab5cd148ea
Son2kwon <101979321+Son2kwon@users.noreply.github.com>
2026-05-16 22:36:24 +0900

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        arr = [0] * 26
+
+        for c in s:
+            arr[ord(c) - ord('a')] += 1
+        
+        for c in t:
+            arr[ord(c) - ord('a')] -= 1
+
+        for i in arr:
+            if i != 0:
+                return False
+
+        return True
+
+# Time Complexity: O(min(n, m)): n, m is a length of string
+# Space Complexity: O(1)
