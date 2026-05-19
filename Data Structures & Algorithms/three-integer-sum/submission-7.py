18a09db572d45f54f5d4203048e37184de7c6e1d
Son2kwon <101979321+Son2kwon@users.noreply.github.com>
2026-05-19 21:35:03 +0900

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,37 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort(); n = len(nums)
+        ans = []
+
+        for i in range(0, n):
+            if i > 0 and nums[i] == nums[i - 1]: continue
+            left = i + 1; right = n - 1;
+            target = -nums[i]
+
+            while left < right:
+                val = nums[left] + nums[right]
+
+                if val == target:
+                    cur = [nums[left], nums[right], nums[i]]
+                    ans.append([nums[left], nums[right], nums[i]])
+                    
+                    cur_left = nums[left]; cur_right = nums[right];
+
+                    while left < n and nums[left] == cur_left:
+                        left += 1;
+                    while right > -1 and nums[right] == cur_right:
+                        right -= 1;
+
+
+                elif val < target: 
+                    left += 1
+                else:
+                    right -= 1
+
+        return ans
+
+
+# 모든 조합을 다 훑는 방법도 있는데, 그건 O(n^3)으로 가니까 쓰면 안 돼.
+# Sorting 후, 기준값을 정한다. 그 기준값보다 오른쪽 배열에서 조합을 찾는다.
+# Time Complexity: O(n^2)
+# Space Complexity: O(n)
