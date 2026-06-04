8234c0338f38e42bb7f0df86c6e46dcac1e12d17
Son2kwon <101979321+Son2kwon@users.noreply.github.com>
2026-06-04 15:26:32 +0900

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        dq = deque()
+        cur = head
+
+        while cur:
+            dq.append(cur)
+            cur = cur.next
+
+        cur = ListNode(-1, head)
+
+        while dq:
+            n1 = dq.popleft()
+            n2 = dq.pop() if dq else None
+
+            cur.next = n1
+            if n2: 
+                n1.next = n2
+                cur = n2
+            else:
+                cur = n1
+            cur.next = None
+
+    
+# 이것도 요리조리 next를 잘 기워넣으면 될 것 같은데
+# Doubly Linked List 면 진짜 훨씬 쉬울텐데...
+# Deque를 활용한다면 앞 뒤에서 한 번 씩 뽑으면 연결 될 것 같은데
+
+# Time Limit이 걸리네. O(n)일텐데.
+# 로직이 문제가 아니라, Output이 문제였구나
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
