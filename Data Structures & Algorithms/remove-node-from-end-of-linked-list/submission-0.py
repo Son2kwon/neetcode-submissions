a3526e0b694fd8cabc17df950843cfcba5f08462
Son2kwon <101979321+Son2kwon@users.noreply.github.com>
2026-06-05 18:45:45 +0900

    Add: remove-node-from-end-of-linked-list - submission-0

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        a: Optional[ListNode] = head
+
+        for i in range(0, n):
+            a = a.next
+
+        b: Optional[ListNode] = head
+        prev = ListNode(-1, None)
+
+        while a:
+            a = a.next
+            prev = b
+            b = b.next
+
+        prev.next = b.next
+        if b == head:
+            head = b.next
+
+        return head
+        
+
+# 하필 end에서부터 n번째 node를 삭제하라는...
+# stack을 사용하면 space complexity O(n)으로 풀 수는 있겠다.
+# 하지만 space complexity를 O(1)으로 풀려면...
+
+# length - n = 지워야 할 index 번호 (0-indexed)
+# O(m)으로 한 번 돌면서 length를 구한 후 (m: 데이터 개수)
+# index를 구해서 지운다
+# O(m)의 시간 복잡도, 2-way
+
+# n 만큼 앞서서 출발하는 포인터 a와 head에서 출발하는 포인터 b
+# a가 None이 될 때까지 옮기면 b는 딱 지우는 위치에서 멈춘다.
+# 결국 O(m)의 시간 복잡도, 2-way
+# 이 방법이 더 섹시한 것 같으니까, 이 방법으로 풀어본다.
