9b0966e1b05fd0a73cbda1d188816488d5d994ca
Son Ui Kwon <101979321+Son2kwon@users.noreply.github.com>
2026-06-12 17:12:50 +0900

    Add: two-integer-sum - submission-13

---
+++ Data Structures & Algorithms/two-integer-sum/submission-13.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict(); n = len(nums)
+
+        for i in range(0, n):
+            t = target - nums[i]
+
+            if nums[i] in d:
+                return [d[nums[i]], i]
+            
+            d[t] = i
+        
+# 26.06.12 복습
+
+# target - nums[i] = (우리가 필요한 숫자) 이므로 이 내용을 딕셔너리에 넣는다.
+#   key: 필요한 숫자, value: 인덱스
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
