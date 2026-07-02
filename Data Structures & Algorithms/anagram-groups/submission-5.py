c5c66a09c9cf8d8b187316793dc0c8dd3292b67a
Son Ui Kwon <101979321+Son2kwon@users.noreply.github.com>
2026-07-02 21:22:17 +0900

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        counter = [0] * 26
+        d = defaultdict(list)
+
+        for s in strs:
+            for c in s:
+                counter[ord(c) - ord('a')] += 1
+
+            k = tuple(counter)
+            d[k].append(s)
+
+            counter = [0] * 26
+
+        ans = []
+        for v in d.values():
+            ans.append(v)
+
+        return ans
+
+# dictionary의 key를 counter로, value를 List[str]으로 하면...
+# 단, dictionary의 key는 변치않는 값이어야 하기 때문에 튜플을 key로 해야 함.
+
+# Counter 끼리의 비교는 잘 하는데, tuple로 묶으니까 같아도 비교를 못 한다.
+# 그냥 Counter를 쓰지 말고 26개 밖에 안 되는데, 그냥 자체적으로 counter를 만들까..?
+
+# Time Complexity: O(m); m is the sum of lengths of strings
+# Space Complexity: O(n); n is the number of strings
