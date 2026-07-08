b9b2ac53ba381685a8847b08f5a25d7406fd3a30
Son Ui Kwon <101979321+Son2kwon@users.noreply.github.com>
2026-07-08 17:19:53 +0900

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.py
@@ -0,0 +1,22 @@
+class KthLargest:
+    heap: list
+    k: int
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = []
+
+        for n in nums:
+            self.heap.append(n)
+
+        self.k = k
+
+    def add(self, val: int) -> int:
+        self.heap.append(val)
+        return heapq.nlargest(self.k, self.heap)[self.k - 1]
+
+# 힙을 써서 푼다면..
+
+# 제일 처음 들어온 list를 힙으로 만들고
+# 들어올 때마다 heapify 하면
+
+# 파이썬은 최대힙을 따로 제공하지 않기 때문에, 모든 값들을 음수로 표현해서 저장한다.
